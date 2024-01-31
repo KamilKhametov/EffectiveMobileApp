88edf0580811142c2a381e4719fade7fc79967fe
KamilKhametov <khametovkamil93@gmail.com>
2024-01-31 13:27:12 +0300

    Реализация buildSrc

---
+++ app/build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("kotlinx-serialization")
+}
+
+android {
+
+    namespace = Config.packageName
+
+    compileSdk = 31
+    buildToolsVersion = "30.0.3"
+
+    defaultConfig {
+        applicationId = Config.packageName
+        minSdk = Config.minSdk
+        targetSdk = Config.targetSdk
+        versionCode = Config.versionCode
+        versionName = Config.versionName
+    }
+
+    buildTypes {
+
+        named("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isDebuggable = false
+            proguardFiles(getDefaultProguardFile(name = "proguard-android.txt"), "proguard-rules.pro")
+        }
+
+        named("debug") {
+            isDebuggable = true
+            isShrinkResources = false
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(Dependencies.coreKtx)
+    implementation(Dependencies.appCompat)
+    implementation(Dependencies.material)
+    implementation(Dependencies.constraint)
+    implementation(Dependencies.coroutinesAndroid)
+    implementation(Dependencies.fragmentKtx)
+    implementation(Dependencies.recyclerView)
+    implementation(Dependencies.dataStore)
+    implementation(Dependencies.viewBinding)
+    implementation(Dependencies.cicerone)
+    implementation(Dependencies.lifecycleViewModel)
+    implementation(Dependencies.lifecycleRuntime)
+    implementation(Dependencies.retrofit)
+    implementation(Dependencies.okhttp)
+    implementation(Dependencies.okhttpInterceptor)
+    implementation(Dependencies.coil)
+    implementation(Dependencies.serializationJson)
+    implementation(Dependencies.serializationConverter)
+    implementation(Dependencies.dagger)
+    implementation(Dependencies.tinkoffPhoneMask)
+
+    kapt(Dependencies.daggerCompiler)
+}
---
+++ buildSrc/src/main/java/Version.kt
@@ -0,0 +1,27 @@
+object Version {
+
+    const val kotlin = "1.6.10"
+    const val gradle = "7.1.2"
+    const val googleServices = "4.3.15"
+
+    const val coreKtx = "1.7.0"
+    const val appCompat = "1.4.1"
+    const val material = "1.4.0"
+    const val constraint = "2.1.3"
+    const val fragmentKtx = "1.4.1"
+    const val recyclerView = "1.2.1"
+    const val dataStore = "1.0.0"
+    const val coroutines = "1.6.4"
+    const val viewBinding = "1.5.6"
+    const val cicerone = "7.1"
+    const val lifecycle = "2.4.1"
+    const val retrofit = "2.9.0"
+    const val okhttp = "4.9.3"
+    const val coil = "1.4.0"
+    const val serializationJson = "1.3.2"
+    const val serializationConverter = "0.8.0"
+    const val dagger = "2.41"
+    const val tinkoffPhoneMask = "1.5.1"
+    const val fbMessaging = "23.1.2"
+    const val barcode = "4.3.0"
+}
